7c66df3fd97330865269c796c4d9bf852b24541f
Viktor <viktor.csanak@gmail.com>
2026-04-26 22:53:53 +0200

    feat(notification): produce kafka messages on user creation

---
+++ user-service/src/main/java/com/example/userservice/config/kafka/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.example.userservice.config.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+  public static final String USER_CREATED_TOPIC = "user.account.created";
+
+  @Bean
+  public NewTopic userCreatedTopic() {
+    return new NewTopic(USER_CREATED_TOPIC, 1, (short) 1);
+  }
+
+  @Bean
+  public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+      kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
+
+    final ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+
+    factory.setConsumerFactory(consumerFactory);
+    return factory;
+  }
+}
--- user-service/src/main/java/com/example/userservice/user/UserService.java
+++ user-service/src/main/java/com/example/userservice/user/UserService.java
@@ -2,22 +2,29 @@ package com.example.userservice.user;
 
 import com.example.userservice.common.exception.UserExistsException;
 import com.example.userservice.common.exception.UserNotFoundException;
+import com.example.userservice.kafka.KafkaProducer;
+import com.example.userservice.notification.dto.UserCreatedMessage;
 import com.example.userservice.user.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
   private final UserRepository repo;
+  private final KafkaProducer producer;
 
-  public UserService(UserRepository repo) {
+  public UserService(UserRepository repo, KafkaProducer producer) {
     this.repo = repo;
+    this.producer = producer;
   }
 
   public User register(User user) {
     if (repo.findByEmail(user.getEmail()).isPresent()) {
       throw new UserExistsException("User already exists");
     }
-    return repo.save(user);
+    repo.save(user);
+    producer.sendUserCreatedMessage(
+        new UserCreatedMessage(user.getEmail(), user.getFirstName(), user.getLastName()));
+    return user;
   }
 
   public User getUserById(Integer id) {
