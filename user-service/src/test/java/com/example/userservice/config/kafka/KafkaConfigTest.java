a7a5f509a337e0d966189058ff97a99e5e78a6ce
Viktor <viktor.csanak@gmail.com>
2026-05-16 11:11:51 +0200

    fix(KafkaConfigTest): replace mocked instance without stubs

--- user-service/src/test/java/com/example/userservice/config/kafka/KafkaConfigTest.java
+++ user-service/src/test/java/com/example/userservice/config/kafka/KafkaConfigTest.java
@@ -5,12 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class KafkaConfigTest {
-  @Mock private KafkaConfig kafkaConfig;
+  private final KafkaConfig kafkaConfig = new KafkaConfig();
 
   @Test
   void userCreatedDltTopicUsesDltName() {
